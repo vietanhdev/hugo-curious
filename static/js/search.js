48397fca7045983a47c5aa7f396f1d24f17c9d60
Viet Anh Nguyen <vietanh.dev@gmail.com>
2020-10-31 19:06:22 +0700

    Placeholder for searchbox

--- static/js/search.js
+++ static/js/search.js
@@ -32,8 +32,6 @@ var searchQuery = param("s");
 if (searchQuery) {
     $("#search-query").val(searchQuery);
     executeSearch(searchQuery);
-} else {
-    $('#search-results').append("<p>Please enter a word or phrase above</p>");
 }
 
 function executeSearch(searchQuery) {
