cda7899a7f66ae3bc7cdd687ec2cc2b181891ec5
Viet Anh Nguyen <vietanh.dev@gmail.com>
2020-11-05 22:23:45 +0700

    Auto hide navbar when scrolling down

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,13 @@
 // add custom js in this file
+
+// Auto hide navbar when scrolling down
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("top-navbar").style.top = "0";
+  } else {
+    document.getElementById("top-navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
